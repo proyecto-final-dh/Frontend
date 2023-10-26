a677bd8af7ced730d620cac3dcaf96e4283bd19a
Jesica Muñoz <jesydynathos@gmail.com>
2023-10-26 12:24:16 -0300

    feat(logo): adding component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { Box } from '@mui/material';
+import blackH from '../../assets/logo_black_H.png';
+import blackV from '../../assets/logo_black_V.png';
+import whiteH from '../../assets/logo_white_H.png';
+import whiteV from '../../assets/logo_white_V.png';
+
+export interface LogoProps {
+  width: number;
+  height: number;
+  variant: 'blackV' | 'whiteV' | 'blackH' | 'whiteH';
+}
+
+const Logo: FC<LogoProps> = ({ variant, width, height }) => {
+  const logo = { blackV, whiteV, blackH, whiteH }[variant];
+  return (
+    <>
+      <Box sx={{ padding: '10px' }}>
+        <img src={logo} alt='logo resqpet' width={width} height={height} />
+      </Box>
+    </>
+  );
+};
+
+export default Logo;
