9b42122717c5dcc4f7ceaab5552acbc2e6810236
Jesica Muñoz <jesydynathos@gmail.com>
2023-11-04 16:22:14 -0300

    feat(petDetail): initial setup for pet detail page

---
+++ src/pages/PetDetail/Pet_detail.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Pet_detail = () => {
+  return (
+    <div>
+      {/*  componente de detalle de mascota JESS */}
+      {/* Banner con frase inspiradora JESYN  */}
+      {/* mascotas de interes JESYN */}
+    </div>
+  );
+};
+
+export default Pet_detail;
