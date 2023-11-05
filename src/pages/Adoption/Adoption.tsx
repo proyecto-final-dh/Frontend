aff89a276d303b8b3ce16a62fca356d1e24f28e1
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2023-11-05 00:14:47 -0500

    feat(Adoption): adding scaffolding

---
+++ src/pages/Adoption/Adoption.tsx
@@ -0,0 +1,18 @@
+type AdoptionProps = {
+  pages: number;
+};
+
+const Adoption = ({ pages }: AdoptionProps) => {
+  console.log({ pages });
+  return (
+    <div className='p-3 col-span-full'>
+      {/* Junior section */}
+      {/* End Junior Section */}
+
+      {/* Felipe section */}
+      {/* End Felipe Section */}
+    </div>
+  );
+};
+
+export default Adoption;
