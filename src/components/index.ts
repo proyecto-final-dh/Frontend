6c701170ec64bb01ed3edad24c1d31d980a05c8d
Jesica Muñoz <jesydynathos@gmail.com>
2023-11-07 00:17:38 -0300

    feat(heroBanner): adding hero banner component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 export { default as Navbar } from './Navbar';
 export { default as Layout } from './Layout';
 export { default as DrawerMenu } from './Drawer';
-export { default as MainBanner } from './Banner';
+export { default as MainBanner } from './Banner/MainBanner';
+export { default as HeroBanner } from './Banner/HeroBanner';
 export { default as Container } from './Container';
 export { default as Title } from './Title';
 export { default as TextDetail } from './TextDetail';
