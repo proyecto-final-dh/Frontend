1f2df3aa9ef261c0fdb286f7def1ae69ad12b72a
Jesica Muñoz <jesydynathos@gmail.com>
2023-11-07 00:19:43 -0300

    feat(home): changing hero banner component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,13 @@ import QrSteps from './components/QrSteps/QrSteps';
 import { MainBanner, Title } from '../../components';
 import imgBanner from '../../assets/banner-init.png';
 import { Box } from '@mui/material';
-import Banner from './components/Banner/Banner';
 import Carrusel from './components/Carrusel/Carrusel';
 import { petData } from '../../data/petData';
+import HeroBanner from '../../components/Banner/HeroBanner';
+import qr from '../../../src/assets/qr-code-movil.png';
+import dog1 from '../../../src/assets/dog-glasses1.png';
+import { relatedPets } from '../../data/relatedPets';
+import RelatedPets from '../PetDetail/components/RelatedPets';
 
 const Home = () => {
   return (
@@ -12,7 +16,12 @@ const Home = () => {
       <Box>
         <MainBanner images={imgBanner} />
         <QrSteps />
-        <Banner />
+        <HeroBanner leftImage={dog1} leftAlt='dog-banner' rightImage={qr} rightAlt='Image-QR'>
+          <Title variant='h2' className='font-bold text-center text-black'>
+            Todo esto
+            <span className='text-white'>sin instalar nada</span>, en un QR que podes agregar al collar de tu mascota.
+          </Title>
+        </HeroBanner>
         <Title variant='h1' className='mt-10 mb-5 font-bold text-center'>
           ADOPCIONES
         </Title>
