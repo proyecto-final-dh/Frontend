be6e4b16ba2f45d639c3953a98cca4adc9171a82
jessicaoo-git <jessi-0517@hotmail.com>
2023-11-08 22:33:54 -0500

    feat: mearge resolve conflicts into pet-detail-page

--- src/data/relatedPets.ts
+++ src/data/relatedPets.ts
@@ -1,26 +1,124 @@
 export const relatedPets = [
   {
-    id: '1',
+    id: 1,
+    name: 'Buddy',
+    status: 'adoptado',
+    size: 'Mediano',
+    gender: 'Macho',
+    description: 'Buddy es un perro cariñoso y juguetón que adora correr por el parque.',
+    image: [
+      {
+        id: 1,
+        alt: 'string',
+        url: 'https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      },
+      {
+        id: 2,
+        alt: 'string',
+        url: 'https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      },
+      {
+        id: 3,
+        alt: 'string',
+        url: 'https://images.unsplash.com/photo-1599839575945-a9e5af0c3fa5?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      },
+    ],
+    breed: {
+      id: 21,
+      name: 'Mestizo',
+      species: {
+        id: 2,
+        name: 'perro',
+      },
+    },
+    userDetails: {
+      id: 1,
+      userId: 'b1c7d38f-6a92-412d-9e81',
+      cellphone: '555-1111',
+      location: {
+        id: 1,
+        country: 'Colombia',
+        city: 'Bogota',
+        state: 'BogotÃ¡',
+      },
+    },
+  },
+  {
+    id: 2,
+    name: 'Luna',
+    status: 'adoptado',
+    size: 'Pequeño',
+    gender: 'Hembra',
+    description: 'Luna es una gatita elegante y curiosa que le encanta explorar la casa.',
+    image: [
+      {
+        id: 1,
+        alt: 'string',
+        url: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      },
+    ],
+    breed: {
+      id: 32,
+      name: 'Angora turco',
+      species: {
+        id: 1,
+        name: 'gato',
+      },
+    },
+    userDetails: {
+      id: 2,
+      userId: 'd0e4f6a5-3c78-8421-9eaa',
+      cellphone: '555-2222',
+      location: {
+        id: 2,
+        country: 'Colombia',
+        city: 'Timbio',
+        state: 'Cauca',
+      },
+    },
+  },
+  {
+    id: 3,
     name: 'Max',
-    image:
-      'https://images.unsplash.com/photo-1561037404-61cd46aa615b?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description:
-      'Soy un perro naturalmente juguetón, curioso y de confianza. Llévame a una caminata larga todos los días, dame algo que hacer. Después de que mi trabajo está hecho, me acurrucaré contigo frente al fuego en las tardes.',
-  },
-  {
-    id: '2',
-    name: 'Barbie',
-    image:
-      'https://images.unsplash.com/photo-1543466835-00a7907e9de1?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8fDB8fHww',
-    description:
-      'Yo tengo todo lo que buscas: soy inteligente y peludo, tengo 4 patas, amo aprender y vivo para complacer. Adelante, enséñame lo que quieras. Sentarse, quedarse quieto, balancear tu chequera, yo puedo hacerlo todo. Mantenme entretenido y seré tuyo para siempre.',
-  },
-  {
-    id: '3',
-    name: 'Chihuahua',
-    image:
-      'https://images.unsplash.com/photo-1607386176712-d8baeb6178a7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description:
-      '¿Buscas una relación emocionalmente segura, mutuamente satisfactoria y de poco mantenimiento? Yo soy lo que necesitas. Permíteme sentarme a tus pies, caminar a tu lado, y siempre seré tu devoto compañero..',
+    status: 'adoptado',
+    size: 'Grande',
+    gender: 'Macho',
+    description: 'Max es un perro grande y amigable que siempre estÃ¡ listo para pasear.',
+    image: [
+      {
+        id: 1,
+        alt: 'string',
+        url: 'https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      },
+      {
+        id: 2,
+        alt: 'string',
+        url: 'https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      },
+      {
+        id: 3,
+        alt: 'string',
+        url: 'https://images.unsplash.com/photo-1599839575945-a9e5af0c3fa5?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      },
+    ],
+    breed: {
+      id: 21,
+      name: 'Mestizo',
+      species: {
+        id: 2,
+        name: 'perro',
+      },
+    },
+    userDetails: {
+      id: 3,
+      userId: 'f9e2d5c1-6b47-129d-8a8e',
+      cellphone: '555-3333',
+      location: {
+        id: 3,
+        country: 'Colombia',
+        city: 'Medellin',
+        state: 'Antioquia',
+      },
+    },
   },
 ];
