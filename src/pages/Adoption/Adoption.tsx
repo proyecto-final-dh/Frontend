c366a52f691ddb25a00d262bf276bcd26511a6b6
Junior Naranjo <juniornaranjo@gmail.com>
2023-11-13 12:29:31 -0500

    feat(Adoption): adding funtion filter with location-species and breeds

--- src/pages/Adoption/Adoption.tsx
+++ src/pages/Adoption/Adoption.tsx
@@ -10,6 +10,8 @@ import { Card } from '../../components/Card';
 import useBreakpoint from '../../hooks/use-breakpoint';
 import { getPetsByStatus } from '../PetDetail/services/pet.service';
 import images from './lib/data';
+import { Pet } from '../../contracts/pet';
+import { APIGetBreeds } from '../../contracts/breeds.contract';
 
 const Adoption = () => {
   const EN_ADOPCION = 'EN_ADOPCION';
@@ -21,15 +23,46 @@ const Adoption = () => {
   const { data: species, isLoading: isLoadingSpacies, error: errorSpacies } = useQuery('species', getSpecies);
   const { data: breeds, isLoading: isLoadingBreeds, error: errorBreeds } = useQuery('breeds', getBreeds);
 
+  console.log('pets', pets?.content);
+
   const [location, setLocation] = useState<null | number>(null);
   const [specie, setSpecie] = useState<null | number>(null);
   const [breed, setBreed] = useState<null | number>(null);
+  const [filterPets, setFilterPets] = useState<Pet[]>([]);
+  const [availableBreeds, setAvailableBreeds] = useState<APIGetBreeds>([]);
+
+  const applyFilter = () => {
+    // Filtra los pets segun la opción seleccionada
+    let filterResult = pets?.content ?? [];
+
+    if (location !== null) {
+      filterResult = filterResult?.filter((pet) => pet.userDetails.location.id === location);
+    }
+    if (specie !== null) {
+      filterResult = filterResult?.filter((pet) => pet.breed.species.id === specie);
+    }
+    if (breed !== null) {
+      filterResult = filterResult?.filter((pet) => pet.breed.id === breed);
+    }
+
+    setFilterPets(filterResult);
+  };
 
   useEffect(() => {
+    if (breeds && specie) {
+      const filterBreeds = breeds?.filter((b) => b.species.id === specie) || [];
+      setAvailableBreeds(filterBreeds);
+    } else {
+      // Si no hay una especie seleccionada, muestramos todos los breeds
+      setAvailableBreeds(breeds ?? []);
+    }
+    if (!specie && !breed) {
+      setFilterPets(pets?.content ?? []);
+    }
     if (errorLocations) console.log({ error: errorLocations });
     if (errorSpacies) console.log({ error: errorSpacies });
     if (errorBreeds) console.log({ error: errorBreeds });
-  }, [errorLocations, errorSpacies, errorBreeds]);
+  }, [errorLocations, errorSpacies, errorBreeds, specie, breed, pets]);
 
   const { isLg } = useBreakpoint('lg');
 
@@ -71,14 +104,16 @@ const Adoption = () => {
             <FormControl fullWidth>
               <InputLabel id='breed'>Raza</InputLabel>
               <Select labelId='breed' id='breed' value={breed} label='Raza' onChange={(e) => setBreed(Number(e.target.value))}>
-                {breeds.map((b) => (
+                {availableBreeds.map((b) => (
                   <MenuItem value={b.id} key={b.id}>
                     {b.name}
                   </MenuItem>
                 ))}
               </Select>
             </FormControl>
-            <button className='w-full py-3 text-center rounded-full bg-primary lg:mt-0'>Buscar</button>
+            <button className='w-full py-3 text-center rounded-full bg-primary lg:mt-0' onClick={applyFilter}>
+              Buscar
+            </button>
           </div>
           {pets && (
             <section className='flex flex-col gap-8'>
@@ -93,7 +128,8 @@ const Adoption = () => {
                 />
               </div>
               <section className='flex flex-wrap justify-center px-10 gap-7'>
-                {pets.content.map((pet, index) => (
+                {/* verifica si hay filtros aplicados */}
+                {(filterPets.length > 0 ? filterPets : pets?.content || []).map((pet, index) => (
                   <Card
                     key={pet.id}
                     variant={isLg ? 'm' : 's'}
