fe32deb445cd6b7052d93dd256cc59623c01a913
Jesica Muñoz <jesydynathos@gmail.com>
2023-11-24 14:02:04 -0300

    fix(styles): changing styles and texts in tour steps

--- src/pages/QrGenerator/lib/tour.steps.tsx
+++ src/pages/QrGenerator/lib/tour.steps.tsx
@@ -7,8 +7,7 @@ export const qrGeneratorSteps: StepType[] = [
     selector: '#qr-generator-1-step',
     content: () => (
       <TextDetail size='xs' weight='bold'>
-        ¡Hola! 🐾 Para empezar, necesitamos que completes un formulario con algunas preguntas para conocer mejor a tu mascota. ¡Será divertido conocer más sobre
-        ella! 🐱🐶
+        ¡Hola! 🐾 Para empezar, necesitamos que completes este formulario con algunas preguntas sobre tu mascota. ¡Y obtené tu QR personalizado! 🐱🐶
       </TextDetail>
     ),
   },
@@ -16,7 +15,7 @@ export const qrGeneratorSteps: StepType[] = [
     selector: '#qr-generator-2-step',
     content: () => (
       <TextDetail size='xs' weight='bold'>
-        ¡Luego de esto, verás en tiempo real 🕐 tus cambios en este preview!🐱🐶
+        ¡Podrás ver en tiempo real 🕐 tus cambios en este preview!🐱🐶
       </TextDetail>
     ),
   },
@@ -24,7 +23,7 @@ export const qrGeneratorSteps: StepType[] = [
     selector: '#qr-generator-3-step',
     content: () => (
       <TextDetail size='xs' weight='bold'>
-        🐾 Primero, necesitamos el nombre de tu mascotita. ¿Cómo se llama esa bolita de pelusa tan encantadora? 😊
+        🐾 Primero, necesitamos el nombre de tu mascota. 😊
       </TextDetail>
     ),
   },
@@ -32,8 +31,7 @@ export const qrGeneratorSteps: StepType[] = [
     selector: '#qr-generator-4-step',
     content: () => (
       <TextDetail size='xs' weight='bold'>
-        🐕 Después de conocer el nombre de tu mascotita, ¿podrías indicarnos la raza y especie a la que pertenece? ¡Queremos saber más sobre ese amiguito
-        peludo! 🌈😊
+        🐕 Después te pedimos nos indiques la raza y especie a la que pertenece, ¡Queremos saber más sobre ese amiguito peludo! 🌈😊
       </TextDetail>
     ),
   },
@@ -41,7 +39,7 @@ export const qrGeneratorSteps: StepType[] = [
     selector: '#qr-generator-5-step',
     content: () => (
       <TextDetail size='xs' weight='bold'>
-        🐾¿Podrías adjuntarnos una imagen de tu amiguito peludo? Nos encantaría verlo en acción. 📷😊
+        🐾¿Podes adjuntar una imagen de tu amiguito peludo? 📷😊
       </TextDetail>
     ),
   },
@@ -49,7 +47,7 @@ export const qrGeneratorSteps: StepType[] = [
     selector: '#qr-generator-6-step',
     content: () => (
       <TextDetail size='xs' weight='bold'>
-        ¿Podrías proporcionarnos una encantadora descripción de tu amiguito peludo? ¡Queremos saber más sobre sus travesuras y personalidad! 📖😊
+        ¿Podes agregar una descripción de tu amiguito peludo? ¡Queremos saber más sobre sus travesuras y personalidad! 📖😊
       </TextDetail>
     ),
   },
@@ -58,8 +56,7 @@ export const qrGeneratorSteps: StepType[] = [
     content: () => (
       <div>
         <TextDetail size='xs' weight='bold'>
-          🐾 Después de conocer el nombre de tu mascotita y toda la información sobre raza, especie y una encantadora descripción, ¡vamos a generar un código QR
-          mágico! 🌟 Al escanearlo, encontrarás toda la información de tu peludito. ¡Será genial! 📷✨
+          🐾 Una vez completado todos los campos, ¡vamos a generar un código QR! 🌟 Al escanearlo, encontrarás toda la información de tu peludito 📷✨
         </TextDetail>
         <img src={qrPreview} />
       </div>
