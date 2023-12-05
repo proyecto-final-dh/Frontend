19ecf5a8e64366fe0eaa5418465e03f9c8ad1afd
Junior Naranjo <juniornaranjo@gmail.com>
2023-12-04 23:21:15 -0500

    feat(YourAccount): fix tittle name item menu

--- src/pages/YourAccount/components/MainMenu/mainMenuPages.ts
+++ src/pages/YourAccount/components/MainMenu/mainMenuPages.ts
@@ -2,5 +2,5 @@ export const MAINMENUROUTES: { label: string; route: string }[] = [
   { label: 'Mi perfil', route: '/my-profile' },
   { label: 'Mis mascotas', route: '/my-pets' },
   { label: 'En adopción', route: '/for-adoption' },
-  { label: 'Pendientes de adopción', route: '/adoption-pending' },
+  { label: 'Mis solicitudes de adopción', route: '/adoption-requests' },
 ];
